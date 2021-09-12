4441aef343d715b26d6240cc7def8dcbbb4d07c4
Layik Hama <layik.hama@gmail.com>
2021-09-12 23:40:28 +0100

    Add pf geo/data files, customise app with about page etc. No layer switching yet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Eatlas from 'eatlas';
 
 import './App.css';
+import Header from './Header';
 
 function App() {
+  const gc = "la_code"
+    , gURL = "https://raw.githubusercontent.com/saferactive/tgve/main/las-only-code.geojson"
+    , dURL = "https://raw.githubusercontent.com/saferactive/tgve/main/ksi.csv";
+
+  const eAtlasInstance = (options) => {
+    const { dataURL, geoURL, geoColumn } = options;
+    return (
+      <Eatlas dark={true}
+        geographyColumn={geoColumn}
+        geographyURL={geoURL}
+        defaultURL={dataURL} /> 
+    )
+  }
+  const map = eAtlasInstance({
+    dataURL: dURL,
+    geoURL: gURL,
+    geoColumn: gc
+  })
+
+  const [component, setComponent] = useState(map);
+
   return (
-    <Eatlas defaultURL={process.env.REACT_APP_DEFAULT_URL}/>
+    <>
+      <Header dark={true} 
+        // can we manage without router?
+        switchComponent={(newComponent) => {
+          if(!newComponent) {
+            // reset
+            document.title = "SaferActive"
+            setComponent(map)
+          } else {
+            document.title = "SaferActive | About"
+            setComponent(newComponent)
+          }
+        }}
+        />
+      {component}
+    </>
   );
 }
 
