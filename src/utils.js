a0cf384a7885f363a1b3650614e5bde2a326f72c
Layik Hama <layik.hama@gmail.com>
2021-09-16 14:25:25 +0100

    Layer names #1

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,14 @@
 const layers = [
   {
-    name: "LA Level",
-    description: "Some description",
+    name: "Local Authority Distrcit",
+    description: "",
     gc: "la_code",
     gURL: "https://raw.githubusercontent.com/saferactive/tgve/main/las-only-code.geojson",
     dURL: "https://raw.githubusercontent.com/saferactive/tgve/main/ksi.csv"
   },
   {
-    name: "Police force evel",
-    description: "Some description",
+    name: "Police Force",
+    description: "",
     gc: "police_force",
     gURL: "https://raw.githubusercontent.com/saferactive/tgve/main/pf-only-name.geojson",
     dURL: "https://raw.githubusercontent.com/saferactive/tgve/main/ksi-pf.csv"
