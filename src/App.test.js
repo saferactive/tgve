f09616b1bfc28a9723bab01dcfdadab8126741b5
Layik Hama <layik.hama@gmail.com>
2021-09-22 09:58:36 +0100

    Tests

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,11 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/add data/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders with params', () => {
+  render(<App hideChartGenerator={true}/>);
+  const linkElement = screen.getByText(/add data/i);
+  expect(linkElement).toBeInTheDocument();
+  const noChartGen = screen.queryByText(/Generate graphs/i);
+  expect(noChartGen).toBeNull();
+});
